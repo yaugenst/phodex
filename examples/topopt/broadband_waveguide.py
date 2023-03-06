f3973f0274c83bfdb7555897362e84053e37cf02
Yannick Augenstein <yannick.augenstein@kit.edu>
2023-03-06 14:38:23 +0100

    Make module for optimization callbacks

--- examples/topopt/broadband_waveguide.py
+++ examples/topopt/broadband_waveguide.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python3
 
 import autograd.numpy as np
-import matplotlib.pyplot as plt
-import meep as mp
 import nlopt
-from cycler import cycler
-from loguru import logger
-from matplotlib import gridspec
 
 from phodex.io import filter_stdout
 from phodex.layout.meep import MultiportDevice2D, Port
+from phodex.optim.callbacks import combine, logging_callback, plotting_callback
 from phodex.optim.nlopt import get_epigraph_formulation
-from phodex.plotting import add_legend_grid
 from phodex.topopt.parametrizations import sigmoid_parametrization
 
 
@@ -50,69 +45,14 @@ def main():
     mpa_opt = p.get_optimization_problem(obj_funs)
 
     parametrization = sigmoid_parametrization((p.nx, p.ny), sigma, beta)
+
     state_dict = {"obj_hist": [], "epivar_hist": [], "cur_iter": 0}
+    log_cb = logging_callback(state_dict)
+    plot_cb = plotting_callback(mpa_opt, p, state_dict, output_dir="output")
 
-    def callback(t, v, f0, grad):
-        design = np.real(mpa_opt.sim.get_epsilon()) - p.n_clad**2
-        design /= p.n_core**2 - p.n_clad**2
-        xx, yy, _, _ = mpa_opt.sim.get_array_metadata()
-
-        if not mp.am_master():
-            return  # only do callback stuff on master
-
-        state_dict["obj_hist"].append(f0)
-        state_dict["epivar_hist"].append(t)
-        state_dict["cur_iter"] += 1
-
-        fig = plt.figure(figsize=(9, 6), tight_layout=True)
-        gs = gridspec.GridSpec(2, 2, height_ratios=[2, 1])
-
-        ax00 = fig.add_subplot(gs[0, 0])
-        prop_cycler = cycler(
-            color=plt.cm.inferno(np.linspace(0.1, 0.9, len(p.wavelengths)))
-        ) * cycler(linestyle=["-", "--", ":", "-."][: len(obj_funs)])
-        ax00.set_prop_cycle(prop_cycler)
-        for wvl_id in range(len(p.wavelengths)):
-            for obj_id in range(len(obj_funs)):
-                idx = wvl_id * len(obj_funs) + obj_id
-                ax00.plot(np.asarray(state_dict["obj_hist"])[:, idx], label=" ")
-        ax00.set_xlabel("iteration")
-        ax00.set_ylabel("objectives")
-        ax00.set_yscale("log")
-
-        # legend
-        row_names = [f"{int(1000 * w)} nm" for w in p.wavelengths]
-        col_names = [f.__name__ for f in obj_funs]
-        legend_ax = fig.add_subplot(gs[1, :])
-        handles, labels = ax00.get_legend_handles_labels()
-        add_legend_grid(handles, labels, row_names, col_names, legend_ax)
-
-        # epigraph variable
-        ax01 = ax00.twinx()
-        color = "tab:blue"
-        ax01.plot(state_dict["epivar_hist"], color=color)
-        ax01.tick_params(axis="y", labelcolor=color)
-        ax01.set_ylabel("epigraph dummy", color=color)
-
-        # device
-        ax1 = fig.add_subplot(gs[0, 1])
-        ax1.pcolormesh(xx, yy, design.T, cmap="gray_r", vmin=0, vmax=1)
-        ax1.set_aspect("equal")
-        ax1.set_xlabel("x (μm)")
-        ax1.set_ylabel("y (μm)")
-
-        fig.savefig(
-            f'output/out{state_dict["cur_iter"]-1:03d}.png', bbox_inches="tight"
-        )
-        plt.close()
-
-        logger.info(
-            f'iteration: {state_dict["cur_iter"]-1:3d}, t: {t:11.4e}, objective (dB): '
-            "[" + ", ".join(f"{10*np.log10(ff):6.2f}" for ff in f0) + "]",
-            flush=True,
-        )
-
-    nlopt_obj, epi_cst = get_epigraph_formulation(mpa_opt, parametrization, callback)
+    nlopt_obj, epi_cst = get_epigraph_formulation(
+        mpa_opt, parametrization, combine(log_cb, plot_cb)
+    )
     epi_tol = np.full(len(obj_funs) * p.nfreq, 1e-4)
 
     n = p.nx * p.ny + 1
@@ -131,7 +71,7 @@ def main():
     opt.set_min_objective(nlopt_obj)
     opt.add_inequality_mconstraint(epi_cst, epi_tol)
     opt.set_param("dual_ftol_rel", 1e-7)
-    opt.set_maxeval(100)
+    opt.set_maxeval(1)
 
     with filter_stdout("phodex"):
         x0[:] = opt.optimize(x0)
---
+++ phodex/optim/callbacks.py
@@ -0,0 +1,114 @@
+from pathlib import Path
+from typing import Callable, Iterable, TypedDict
+
+import matplotlib.pyplot as plt
+import meep.adjoint as mpa
+import numpy as np
+from cycler import cycler
+from loguru import logger
+from matplotlib import figure, gridspec
+
+from phodex.layout.meep import MultiportDevice2D
+from phodex.plotting import add_legend_grid
+
+
+class StateDict(TypedDict):
+    obj_hist: list[Iterable[float]]
+    epivar_hist: list[float]
+    cur_iter: int
+
+
+def combine(*functions: Callable) -> Callable:
+    def _combined(*args, **kwargs):
+        [f(*args, **kwargs) for f in functions]
+
+    return _combined
+
+
+def logging_callback(state_dict: StateDict | None = None) -> Callable:
+    if state_dict is None:
+        state_dict = {"obj_hist": [], "epivar_hist": [], "cur_iter": 0}
+
+    def _callback(t, v, f0, grad) -> None:
+        state_dict["obj_hist"].append(f0)
+        state_dict["epivar_hist"].append(t)
+        state_dict["cur_iter"] += 1
+        logger.info(
+            f'iteration: {state_dict["cur_iter"]-1:3d}, t: {t:11.4e}, objective (dB): '
+            "[" + ", ".join(f"{10*np.log10(ff):6.2f}" for ff in f0) + "]",
+            flush=True,
+        )
+
+    return _callback
+
+
+def plotting_callback(
+    mpa_opt: mpa.OptimizationProblem,
+    device: MultiportDevice2D,
+    state_dict: StateDict,
+    fig: figure.Figure | None = None,
+    output_dir: Path | str | None = None,
+) -> Callable:
+    obj_funs = mpa_opt.objective_functions
+    nrows = len(device.wavelengths)
+    ncols = len(obj_funs)
+
+    if output_dir is not None:
+        output_dir = Path(output_dir)
+        output_dir.mkdir(parents=True, exist_ok=True)
+        logger.info(f"Saving plots to {output_dir.resolve()}.")
+
+    if fig is None:
+        fig = plt.figure(figsize=(9, 6), tight_layout=True)
+
+    def _callback(t, v, f0, grad):
+        design = np.real(mpa_opt.sim.get_epsilon()) - device.n_clad**2
+        design /= device.n_core**2 - device.n_clad**2
+        xx, yy, _, _ = mpa_opt.sim.get_array_metadata()
+
+        gs = gridspec.GridSpec(2, 2, height_ratios=[2, 1])
+
+        ax00 = fig.add_subplot(gs[0, 0])
+        prop_cycler = cycler(
+            color=plt.cm.inferno(np.linspace(0.1, 0.9, nrows))
+        ) * cycler(linestyle=["-", "--", ":", "-."][:ncols])
+        ax00.set_prop_cycle(prop_cycler)
+        for wvl_id in range(nrows):
+            for obj_id in range(ncols):
+                idx = wvl_id * ncols + obj_id
+                ax00.plot(np.asarray(state_dict["obj_hist"])[:, idx], label=" ")
+        ax00.set_xlabel("iteration")
+        ax00.set_ylabel("objectives")
+        ax00.set_yscale("log")
+
+        # legend
+        row_names = [f"{int(1000 * w)} nm" for w in device.wavelengths]
+        col_names = [f.__name__ for f in obj_funs]
+        legend_ax = fig.add_subplot(gs[1, :])
+        handles, labels = ax00.get_legend_handles_labels()
+        add_legend_grid(handles, labels, row_names, col_names, legend_ax)
+
+        # epigraph variable
+        ax01 = ax00.twinx()
+        color = "tab:blue"
+        ax01.plot(state_dict["epivar_hist"], color=color)
+        ax01.tick_params(axis="y", labelcolor=color)
+        ax01.set_ylabel("epigraph dummy", color=color)
+
+        # device
+        ax1 = fig.add_subplot(gs[0, 1])
+        ax1.pcolormesh(xx, yy, design.T, cmap="gray_r", vmin=0, vmax=1)
+        ax1.set_aspect("equal")
+        ax1.set_xlabel("x (μm)")
+        ax1.set_ylabel("y (μm)")
+
+        if output_dir is not None:
+            fig.savefig(
+                output_dir / f'out{state_dict["cur_iter"]-1:03d}.png',
+                bbox_inches="tight",
+                pad_inches=0.1,
+            )
+
+        plt.close()
+
+    return _callback
