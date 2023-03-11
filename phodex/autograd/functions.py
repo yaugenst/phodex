0c55e8aea24b32c9238508043d2f79294e8241b9
Yannick Augenstein <yannick.augenstein@kit.edu>
2023-03-11 18:08:18 +0100

    Add convolution with different padding modes

--- phodex/autograd/functions.py
+++ phodex/autograd/functions.py
@@ -1,6 +1,8 @@
-from typing import Iterable
+from typing import Iterable, Literal
 
 import autograd.numpy as np
+from autograd.scipy.signal import convolve as convolve_ag
+
 
 def edge_pad(x: np.ndarray, pad: int, axis: int | Iterable | None = None) -> np.ndarray:
     if axis is None:
@@ -67,4 +69,36 @@ def circular_pad(
         )
         x = np.concatenate([x[idx], x], axis=ax)
 
+    return x
+
+
+def convolve(
+    x: np.ndarray,
+    k: np.ndarray,
+    mode: Literal["constant", "edge", "reflect", "wrap"] = "constant",
+) -> np.ndarray:
+    if len(set(k.shape)) > 1:
+        raise NotImplementedError(
+            f"Non-square kernels not implemented yet! Got {k.shape}"
+        )
+    if k.shape[0] % 2 == 0:
+        raise NotImplementedError(
+            f"Even-sized kernels not supported yet! Got {k.shape}"
+        )
+
+    p = k.shape[0] // 2
+
+    match mode:
+        case "constant":
+            x = np.pad(x, p, mode="constant")
+        case "edge":
+            x = edge_pad(x, p)
+        case "reflect":
+            x = reflection_pad(x, p)
+        case "wrap":
+            x = circular_pad(x, p)
+        case _:
+            raise ValueError(f"Unsupported padding mode: {mode}")
+
+    x = convolve_ag(x, k, mode="valid")
     return x
