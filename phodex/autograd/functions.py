912bd224086a81343c692d9fe89ebd16df5ca569
Yannick Augenstein <yannick.augenstein@kit.edu>
2023-03-11 18:04:57 +0100

    Add edge padding

--- phodex/autograd/functions.py
+++ phodex/autograd/functions.py
@@ -2,6 +2,24 @@ from typing import Iterable
 
 import autograd.numpy as np
 
+def edge_pad(x: np.ndarray, pad: int, axis: int | Iterable | None = None) -> np.ndarray:
+    if axis is None:
+        axis = list(range(x.ndim))
+    if isinstance(axis, int):
+        axis = [axis]
+
+    for ax in axis:
+        if ax >= len(x.shape):
+            raise IndexError(f"axis {ax} out of range")
+
+        idx = tuple(slice(None) if s != ax else -1 for s in range(len(x.shape)))
+        x = np.concatenate([x, np.stack(pad // 2 * [x[idx]], axis=ax)], axis=ax)
+
+        idx = tuple(slice(None) if s != ax else 0 for s in range(len(x.shape)))
+        x = np.concatenate([np.stack(pad // 2 * [x[idx]], axis=ax), x], axis=ax)
+
+    return x
+
 
 def reflection_pad(
     x: np.ndarray, pad: int, axis: int | Iterable | None = None
