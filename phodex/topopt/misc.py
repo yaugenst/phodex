a164c2fbf3f5ab27b2fc77c626637f08f96032d9
Yannick Augenstein <yannick.augenstein@kit.edu>
2023-03-11 15:39:57 +0100

    Add sigma calculation from feature size for gaussian kernel (approximate)

--- phodex/topopt/misc.py
+++ phodex/topopt/misc.py
@@ -3,3 +3,7 @@ import autograd.numpy as np
 
 def gray_indicator(x):
     return np.mean(4 * x * (1 - x))
+
+
+def sigma_from_feature_size(feature_size: float, resolution: int) -> float:
+    return (feature_size * resolution) / np.sqrt(3)
