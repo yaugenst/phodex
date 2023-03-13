289436d40710fe7419f6e8a37a3f0453c95d1762
Yannick Augenstein <yannick.augenstein@kit.edu>
2023-03-13 11:11:09 +0100

    Update parametrizations to support multiple filter types and add type hints

--- phodex/topopt/parametrizations.py
+++ phodex/topopt/parametrizations.py
@@ -1,23 +1,59 @@
+from typing import Callable, Iterable, Literal
+
 import autograd.numpy as np
 
-from phodex.topopt.filters import gaussian_filter
+from phodex.autograd.functions import _pad_modes
+from phodex.topopt.filters import cone_filter, gaussian_filter
 from phodex.topopt.projections import sigmoid, simp
 
 
-def sigmoid_parametrization(shape, sigma, alpha, beta=0.5, flat=True):
-    def _parametrization(x):
+def sigmoid_parametrization(
+    shape: Iterable[int],
+    filter_size: int,
+    projection_strength: float,
+    projection_center: float = 0.5,
+    filter_type: Literal["gaussian", "cone"] = "gaussian",
+    filter_padding: _pad_modes = "symmetric",
+    flat: bool = True,
+) -> Callable:
+    match filter_type:
+        case "gaussian":
+            filt = gaussian_filter(filter_size, mode=filter_padding)
+        case "cone":
+            filt = cone_filter(filter_size, mode=filter_padding)
+        case _:
+            raise ValueError(f"Unkown filter type: {filter_type}.")
+
+    def _parametrization(x: np.ndarray) -> np.ndarray:
         x = np.reshape(x, shape)
-        x = gaussian_filter(x, sigma)
-        x = sigmoid(x, alpha, beta)
+        x = filt(x)
+        x = sigmoid(x, projection_strength, projection_center)
         return x.ravel() if flat else x
 
     return _parametrization
 
 
-def simp_parametrization(shape, sigma, vmin, vmax, penalty=3.0, flat=True):
-    def _parametrization(x):
+def simp_parametrization(
+    shape: Iterable[int],
+    filter_size: int,
+    vmin: float = 0.0,
+    vmax: float = 1.0,
+    penalty: float = 3.0,
+    filter_type: Literal["gaussian", "cone"] = "gaussian",
+    filter_padding: _pad_modes = "symmetric",
+    flat: bool = True,
+) -> Callable:
+    match filter_type:
+        case "gaussian":
+            filt = gaussian_filter(filter_size, mode=filter_padding)
+        case "cone":
+            filt = cone_filter(filter_size, mode=filter_padding)
+        case _:
+            raise ValueError(f"Unkown filter type: {filter_type}.")
+
+    def _parametrization(x: np.ndarray) -> np.ndarray:
         x = np.reshape(x, shape)
-        x = gaussian_filter(x, sigma)
+        x = filt(x)
         x = simp(x, vmin, vmax, penalty)
         return x.ravel() if flat else x
 
